e52036bf93de96dea6c0664458248630d9644727
Jefferson Candido <jefferson.ufu@gmail.com>
2020-08-19 01:39:11 -0300

    Simple CRUD simplelist

--- main.cpp
+++ main.cpp
@@ -1,7 +1,111 @@
 #include <iostream>
+#include <vector>
 
-int main(int argc, char const *argv[])
+using namespace std;
+
+void print_menu(string name);
+void print_list();
+void add_item();
+void delete_item();
+
+vector<string> list;
+string name;
+
+int main(int arg_count, char const *args[])
 {
-    std::cout << "Hello World!" << std::endl;
+    if( arg_count > 1) {
+        name = string(args[1]);
+        print_menu(name);
+    }
+    else
+    {
+        cout << "Username not supplied... exiting the program" << endl;
+    }
+    
     return 0;
+}
+
+void print_menu(string name) {
+    int choice;
+
+    cout << "***********************\n";
+    cout << " 1 - Print list.\n";
+    cout << " 2 - Add to list.\n";
+    cout << " 3 - Delete from list.\n";
+    cout << " 4 - Quit.\n";
+    cout << " Enter your choice and press return: ";
+
+    cin >> choice;
+
+    if (choice == 4){
+        exit(0);
+    }
+    else if ( choice == 2 ) {
+        add_item();
+    }
+    else if ( choice == 3 ) {
+        delete_item();
+    }
+    else if ( choice == 1 ) {
+        print_list();
+    }
+    else {
+        cout << "Sorry, choice not implemented yet";
+    }
+}
+
+void add_item() {
+    cout << "\n\n\n\n\n\n\n";
+    cout << "*** Add Item ***\n";
+    cout << "Type in an item and press enter: ";
+
+    string item;
+    cin >> item;
+
+    list.push_back(item);
+
+    cout << "Successfully added an item to the list \n\n\n";
+    cin.clear();
+
+    print_menu(name);
+}
+
+void delete_item() {
+    cout << "*** Delete item ***\n";
+    cout << "Select an item index number to delete\n";
+    if (list.size()) {
+        for ( int i = 0; i < list.size(); i++ ) {
+            cout << i << ": " << list[i] << "\n";
+        }
+        char choice;
+        cin >> choice;
+        list.erase(list.begin() + choice - 1);
+
+        cout << "Removed list item of index: " << choice << endl;
+    }
+    else {
+        cout << "No items to delete.\n";
+    }
+
+    print_menu(name);
+}
+
+void print_list() {
+    cout << "\n\n\n\n\n\n";
+    cout << "*** Printing List ***\n";
+
+    for ( int list_index = 0; list_index < list.size(); list_index++) {
+        cout << " * " << list[list_index] << "\n";
+    }
+
+    cout << "M - Menu \n";
+    char choice;
+    cin >> choice;
+
+    if ( choice == 'M' || choice == 'm') {
+        print_menu(name);
+    }
+    else {
+        cout << "Invalid choice. Quitting...\n";
+    }
 }
